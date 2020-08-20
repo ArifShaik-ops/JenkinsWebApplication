476cfb138afc8dc766e417a0f9c276b69a8d9fad
arifshaik-ops <arif.shaik.java@gmail.com>
2020-08-20 20:25:02 +0530

    Initial Commit

---
+++ src/main/java/com/axis/main/JenkinsSpringBootSampleApplication.java
@@ -0,0 +1,13 @@
+package com.axis.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsSpringBootSampleApplication.class, args);
+	}
+
+}
